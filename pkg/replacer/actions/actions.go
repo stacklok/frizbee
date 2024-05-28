b5f98fb57eb004e0bcec5057ab5723e481e93f37
Radoslav Dimitrov <radoslav@stacklok.com>
2024-05-29 01:59:39 +0300

    Tidy up the image and actions packages
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- pkg/replacer/actions/actions.go
+++ pkg/replacer/actions/actions.go
@@ -18,8 +18,14 @@ package actions
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
+	"net/url"
 	"strings"
+
+	"github.com/google/go-github/v61/github"
 
 	"github.com/stacklok/frizbee/pkg/interfaces"
 	"github.com/stacklok/frizbee/pkg/replacer/image"
@@ -28,12 +34,19 @@ import (
 )
 
 const (
+	prefixUses   = "uses: "
+	prefixDocker = "docker://"
 	// GitHubActionsRegex is regular expression pattern to match GitHub Actions usage
 	GitHubActionsRegex = `uses:\s*[^\s]+/[^\s]+@[^\s]+|uses:\s*docker://[^\s]+:[^\s]+`
-	prefixUses         = "uses: "
-	prefixDocker       = "docker://"
 	// ReferenceType is the type of the reference
 	ReferenceType = "action"
+)
+
+var (
+	// ErrInvalidAction is returned when parsing the action fails.
+	ErrInvalidAction = errors.New("invalid action")
+	// ErrInvalidActionReference is returned when parsing the action reference fails.
+	ErrInvalidActionReference = errors.New("action reference is not a tag nor branch")
 )
 
 // Parser is a struct to replace action references with digests
@@ -215,4 +228,126 @@ func (_ *Parser) ConvertToEntityRef(reference string) (*interfaces.EntityRef, er
 		Ref:  frags[1],
 		Type: refType,
 	}, nil
+}
+
+// isLocal returns true if the input is a local path.
+func isLocal(input string) bool {
+	return strings.HasPrefix(input, "./") || strings.HasPrefix(input, "../")
+}
+
+func shouldExclude(cfg *config.GHActions, input string) bool {
+	for _, e := range cfg.Exclude {
+		if e == input {
+			return true
+		}
+	}
+	return false
+}
+
+// ParseActionReference parses an action reference into action and reference.
+func ParseActionReference(input string) (action string, reference string, err error) {
+	frags := strings.Split(input, "@")
+	if len(frags) != 2 {
+		return "", "", fmt.Errorf("invalid action reference: %s", input)
+	}
+
+	return frags[0], frags[1], nil
+}
+
+// GetChecksum returns the checksum for a given action and tag.
+func GetChecksum(ctx context.Context, restIf interfaces.REST, action, ref string) (string, error) {
+	owner, repo, err := parseActionFragments(action)
+	if err != nil {
+		return "", err
+	}
+
+	// Check if we're using a checksum
+	if isChecksum(ref) {
+		return ref, nil
+	}
+
+	res, err := getCheckSumForTag(ctx, restIf, owner, repo, ref)
+	if err != nil {
+		return "", fmt.Errorf("failed to get checksum for tag: %w", err)
+	} else if res != "" {
+		return res, nil
+	}
+
+	// check branch
+	res, err = getCheckSumForBranch(ctx, restIf, owner, repo, ref)
+	if err != nil {
+		return "", fmt.Errorf("failed to get checksum for branch: %w", err)
+	} else if res != "" {
+		return res, nil
+	}
+
+	return "", ErrInvalidActionReference
+}
+
+func parseActionFragments(action string) (owner string, repo string, err error) {
+	frags := strings.Split(action, "/")
+
+	// if we have more than 2 fragments, we're probably dealing with
+	// sub-actions, so we take the first two fragments as the owner and repo
+	if len(frags) < 2 {
+		return "", "", fmt.Errorf("%w: '%s' reference is incorrect", ErrInvalidAction, action)
+	}
+
+	return frags[0], frags[1], nil
+}
+
+// isChecksum returns true if the input is a checksum.
+func isChecksum(ref string) bool {
+	return len(ref) == 40
+}
+
+func getCheckSumForTag(ctx context.Context, restIf interfaces.REST, owner, repo, tag string) (string, error) {
+	path, err := url.JoinPath("repos", owner, repo, "git", "refs", "tags", tag)
+	if err != nil {
+		return "", fmt.Errorf("failed to join path: %w", err)
+	}
+
+	return doGetReference(ctx, restIf, path)
+}
+
+func getCheckSumForBranch(ctx context.Context, restIf interfaces.REST, owner, repo, branch string) (string, error) {
+	path, err := url.JoinPath("repos", owner, repo, "git", "refs", "heads", branch)
+	if err != nil {
+		return "", fmt.Errorf("failed to join path: %w", err)
+	}
+
+	return doGetReference(ctx, restIf, path)
+}
+
+func doGetReference(ctx context.Context, restIf interfaces.REST, path string) (string, error) {
+	req, err := restIf.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return "", fmt.Errorf("cannot create REST request: %w", err)
+	}
+
+	resp, err := restIf.Do(ctx, req)
+
+	if resp != nil {
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+	}
+
+	if err != nil && resp.StatusCode != http.StatusNotFound {
+		return "", fmt.Errorf("failed to do API request: %w", err)
+	} else if resp.StatusCode == http.StatusNotFound {
+		// No error, but no tag found
+		return "", nil
+	}
+
+	var t github.Reference
+	err = json.NewDecoder(resp.Body).Decode(&t)
+	if err != nil && strings.Contains(err.Error(), "cannot unmarshal array into Go value of type") {
+		// This is a branch, not a tag
+		return "", nil
+	} else if err != nil {
+		return "", fmt.Errorf("canont decode response: %w", err)
+	}
+
+	return t.GetObject().GetSHA(), nil
 }
--- pkg/replacer/image/image.go
+++ pkg/replacer/image/image.go
@@ -18,9 +18,16 @@ package image
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+
+	"github.com/stacklok/frizbee/internal/cli"
 	"github.com/stacklok/frizbee/pkg/interfaces"
 	"github.com/stacklok/frizbee/pkg/utils/config"
 	"github.com/stacklok/frizbee/pkg/utils/store"
@@ -132,4 +139,69 @@ func (_ *Parser) ConvertToEntityRef(reference string) (*interfaces.EntityRef, er
 		Ref:  frags[1],
 		Type: ReferenceType,
 	}, nil
+}
+
+// GetImageDigestFromRef returns the digest of a container image reference
+// from a name.Reference.
+func GetImageDigestFromRef(ctx context.Context, imageRef, platform string, cache store.RefCacher) (*interfaces.EntityRef, error) {
+	// Parse the image reference
+	ref, err := name.ParseReference(imageRef)
+	if err != nil {
+		return nil, err
+	}
+	opts := []remote.Option{
+		remote.WithContext(ctx),
+		remote.WithUserAgent(cli.UserAgent),
+		remote.WithAuthFromKeychain(authn.DefaultKeychain),
+	}
+
+	// Set the platform if provided
+	if platform != "" {
+		platformSplit := strings.Split(platform, "/")
+		if len(platformSplit) != 2 {
+			return nil, errors.New("platform must be in the format os/arch")
+		}
+		opts = append(opts, remote.WithPlatform(v1.Platform{
+			OS:           platformSplit[0],
+			Architecture: platformSplit[1],
+		}))
+	}
+
+	// Get the digest of the image reference
+	var digest string
+
+	if cache != nil {
+		if d, ok := cache.Load(imageRef); ok {
+			digest = d
+		} else {
+			desc, err := remote.Get(ref, opts...)
+			if err != nil {
+				return nil, err
+			}
+			digest = desc.Digest.String()
+			cache.Store(imageRef, digest)
+		}
+	} else {
+		desc, err := remote.Get(ref, opts...)
+		if err != nil {
+			return nil, err
+		}
+		digest = desc.Digest.String()
+	}
+
+	// Compare the digest with the reference and return the original reference if they already match
+	if digest == ref.Identifier() {
+		return nil, fmt.Errorf("image already referenced by digest: %s %w", imageRef, interfaces.ErrReferenceSkipped)
+	}
+
+	return &interfaces.EntityRef{
+		Name: ref.Context().Name(),
+		Ref:  digest,
+		Type: ReferenceType,
+		Tag:  ref.Identifier(),
+	}, nil
+}
+
+func shouldExclude(ref string) bool {
+	return ref == "scratch"
 }
